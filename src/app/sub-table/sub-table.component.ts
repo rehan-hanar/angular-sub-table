d43add203baf3a7bcaf535aead183fdae4b7609a
rehanangular <rehanhanar.com@gmail.com>
2021-11-08 11:56:11 +0530

    3 files modified

--- src/app/sub-table/sub-table.component.ts
+++ src/app/sub-table/sub-table.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SubTableComponent implements OnInit {
   @Input() data: any;
   @Input() columnsToDisplay: Array<any>;
+  @Input() headersForColumns: any;
   // columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
 
   constructor() {}
